5fd233c37b79326058b7f4eef2c6b3920376e5b9
ikbalaa04 <ikbalaa04@gmail.com>
2026-03-15 19:50:42 +0700

    update

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -13,29 +13,6 @@
         <nav id="navbar" class="navbar">
           <ul>
             <li><a class="nav-link scrollto active" href="<?php echo base_url() ?>">Home</a></li>
-            <li><a class="nav-link scrollto" href="<?php echo base_url('home/lebih_berita') ?>">Informasi</a></li>
-            
-            <li><a class="nav-link scrollto " href="#portfolio">Galeri</a></li>
-            <li><a class="nav-link scrollto" href="<?php echo base_url('home/kelulusan') ?>">Kelulusan</a></li>
-            <li><a class="nav-link scrollto" href="#testimonials">Pendaftar</a></li>
-            <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-              <ul>
-                <li><a href="#">Drop Down 1</a></li>
-                <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                  <ul>
-                    <li><a href="#">Deep Drop Down 1</a></li>
-                    <li><a href="#">Deep Drop Down 2</a></li>
-                    <li><a href="#">Deep Drop Down 3</a></li>
-                    <li><a href="#">Deep Drop Down 4</a></li>
-                    <li><a href="#">Deep Drop Down 5</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Drop Down 2</a></li>
-                <li><a href="#">Drop Down 3</a></li>
-                <li><a href="#">Drop Down 4</a></li>
-              </ul>
-            </li> -->
-            <!-- <li><a class="nav-link scrollto" href="<?php echo base_url('home/lebih_biaya') ?>">Biaya</a></li> -->
             <li><a class="nav-link scrollto" href="<?php echo base_url('home/pendaftaran')?>">Daftar</a></li>
             <li><a class="getstarted scrollto" href="<?php echo base_url('login')?>"><i class="fa fa-sign-in"></i>Login</a></li>
           </ul>
