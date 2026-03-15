14eecae2282a42b0da99a550147ad5d0b48fd942
ikbalaa04 <ikbalaa04@gmail.com>
2026-03-15 22:22:50 +0700

    delete link WA Group

--- application/views/admin/layout/header.php
+++ application/views/admin/layout/header.php
@@ -27,7 +27,7 @@
             <div class="navbar-custom-menu">
               <ul class="nav navbar-nav">
                 <li class="dropdown user user-menu">
-                     <a  href="https://chat.whatsapp.com/CFDbmjiZ8KW7Y4VqsxWkcY" target="_blank" class="btn btn-success btn-flat"><i class="fa fa-whatsapp"></i> Gabung WA Group</a>
+                     <a  href="#" target="_blank" class="btn btn-success btn-flat"><i class="fa fa-whatsapp"></i> Gabung WA Group</a>
                   </li>
                 <!--<li class="dropdown user user-menu">-->
                 <!--     <a  href="https://chat.whatsapp.com/GS1jLrWOnF39NOBKxJCncR" target="_blank" class="btn btn-success btn-flat"><i class="fa fa-whatsapp"></i> WA Group Test Gel. 3</a>-->
