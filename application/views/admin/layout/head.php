5041a3a2b7b84a9c7eff4143ad86ca776d6d60a4
ikbalaa04 <ikbalaa04@gmail.com>
2026-05-29 15:29:24 +0700

    update

--- application/views/admin/layout/head.php
+++ application/views/admin/layout/head.php
@@ -366,9 +366,12 @@ function showUser(str) {
     $(document).on('click', '.btn-next-step', function(){
       var form = $(this).closest('form');
       if (validateRequired(form, true)) {
+        form.data('submit-mode', 'data-diri');
         form.find('input[name="simpan_data_diri"]').remove();
+        form.find('.js-submit-data-diri').remove();
         form.append('<input type="hidden" name="simpan_data_diri" value="1">');
-        HTMLFormElement.prototype.submit.call(form[0]);
+        form.append('<button type="submit" class="js-submit-data-diri" style="display:none"></button>');
+        form.find('.js-submit-data-diri')[0].click();
       }
     });
 
